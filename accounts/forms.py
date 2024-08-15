8b7d1c688db3ac3cefb5ba9652b2dbb241a63b64
Sanjarbek992 <sanjarbekabdullayev992@gmail.com>
2024-08-15 15:03:58 +0500

    MRITda 15.08. da ishlaganim

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,20 +4,30 @@ from .models import Account
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Enter Password',
+        'placeholder': 'Parol',
         'class': 'form-control',
     }))
-    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Parolni tasdiqlash'}))
 
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
 
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Parol to'g'ri kelmadi! Parolni qaytadan kiriting"
+            )
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
-        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
-        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
-        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
-        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Ismingiz'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Familiyangiz'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Telefon raqam'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email adres'
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import RegistrationForm
+from .forms import RegistrationForm, Account
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -9,9 +9,13 @@ def register(request):
             phone_number = form.cleaned_data['phone_number']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
+            username = email.split("@")[0]
 
-            user = Account.objects.create_user6()
-    form = RegistrationForm()
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+    else:
+        form = RegistrationForm()
     context = {
         'form': form
     }
